d3a0eb404c5b467cab24b594ec17e288aabf1e6d
Stellaris <89051147+goodWishesEveryone@users.noreply.github.com>
2021-12-06 20:58:32 -0500

    Modified express_server.js; adding another route response that contain HTML code, which would be rendered in the client browser.

--- express_server.js
+++ express_server.js
@@ -3,8 +3,8 @@ const app = express();
 const PORT = 8080; // default port 8080
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  b2xVn2: "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
@@ -17,6 +17,15 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/set", (req, res) => {
+  const a = 1;
+  res.send(`a = ${a}`);
+});
+
+app.get("/fetch", (req, res) => {
+  res.send(`a = ${a}`);
 });
 
 app.listen(PORT, () => {
